908a1d5adccee06a890899598b0e1c53896f4249
LeRusseBesuhof <p.grad0ff@gmail.com>
2024-06-04 22:56:22 +0700

    v 0.1 (set up first screen)

---
+++ Dev3/Protocol/NetworkProtocol.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkProtocol.swift
+//  Dev3
+//
+//  Created by Павел Градов on 04.06.2024.
+//
+
+import Foundation
---
+++ Dev3/Protocol/ViewControllerProtocol.swift
@@ -0,0 +1,8 @@
+//
+//  ViewControllerProtocol.swift
+//  Dev3
+//
+//  Created by Павел Градов on 04.06.2024.
+//
+
+import Foundation
